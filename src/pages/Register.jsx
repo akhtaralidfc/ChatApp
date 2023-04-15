fd6f032380682b3310d85a469472d70882171eef
Md Akhtar Ali <75729680+akhtaralidfc@users.noreply.github.com>
2023-04-15 14:56:15 +0530

    change heading front p

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -62,7 +62,7 @@ const Register = () => {
   return (
     <div className="formContainer">
       <div className="formWrapper">
-        <span className="logo">FLAT PARTNERS CHAT</span>
+        <span className="logo">CHAT</span>
         <span className="title">Register</span>
         <form onSubmit={handleSubmit}>
           <input required type="text" placeholder="display name" />
